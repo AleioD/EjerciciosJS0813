bb61711b39077a68c97918ceb8acbd03ec28d305
AleioD <alejandrodaquila@hotmail.com>
2019-08-14 14:41:41 -0300

    ejercicios del DOM2 parcial

--- js/gastos.js
+++ js/gastos.js
@@ -27,9 +27,62 @@ window.onload = function () {
       while (isNaN(gastos) || gastos == '') {
         gastos = prompt ('El valor debe ser un número. Ingrese gastos:').trim();
       }
-      arrayFamily[i] = '{nombre: '+ name + ', gastos: ' + gastos + '},'
+      arrayFamily[i] = {
+        nombre: name,
+        gastos: parseInt(gastos)
+      }
     }
+
     console.log(arrayFamily);
-    
+
+
+    document.querySelector ('body').append(document.createElement('h1'));
+    document.querySelector('h1').append(document.createTextNode('Reporte de gastos familiares'));
+
+
+    var max = 0;
+    var maxName = '';
+    var min = 0;
+    var minName = '';
+    var totalGastos = 0;
+    var arrayMensajes = [];
+    for (var onePerson of arrayFamily) {
+      if(max < onePerson.gastos){
+        max = onePerson.gastos;
+        maxName = onePerson.nombre;
+        min = onePerson.gastos;
+      }
+      if(min > onePerson.gastos){
+        min = onePerson.gastos;
+        minName = onePerson.nombre;
+      }
+      totalGastos = totalGastos + onePerson.gastos;
+      var promedio = (totalGastos / countFamily).toFixed(2);
+    }
+    arrayMensajes.push(maxName + ' fue el que más gastó: $ ' + max);
+    arrayMensajes.push(minName + ' fue el que menos gastó: $ ' + min);
+    arrayMensajes.push('El gasto total de la familia fue: $ ' + totalGastos);
+    arrayMensajes.push('El gasto promedio de la familia fue: $ ' + promedio);
+
+    document.querySelector ('body').append(document.createElement('ul'));
+    var ul = document.querySelector ('ul');
+    for (var i = 0; i < arrayMensajes.length; i++) {
+      ul.append(document.createElement('li'));
+    }
+    var losLi = document.querySelectorAll ('li')
+    for (var i = 0; i < losLi.length; i++) {
+      losLi[i].append(arrayMensajes[i]);
+      losLi[i].setAttribute('title', arrayMensajes[i]);
+    }
+
+    document.querySelector ('body').append(document.createElement('button'));
+    document.querySelector('button').append(document.createTextNode('¿Nos pasamos del presupuesto?'));
+    document.querySelector('button').onclick = function(){
+      if (totalGastos > 1200) {
+        alert('EL PRESUPUESTO FUE SUPERADO POR $ ' + (totalGastos - 1200));
+      } else {
+        alert('La familia está dentro de presupuesto');
+      }
+    };
   }
 }
